55d9d80c08de91a73d592061b04d8096c831c911
ziing <2651217350@qq.com>
2016-03-21 15:45:26 +0800

    3-21

---
+++ test.js
@@ -0,0 +1,19 @@
+/**
+ * Created by acer on 2016/3/21.
+ */
+function aa(){
+    $("#yjbtn").css({
+        "background": "yellow",
+        "color":"red",
+        "border":"solid 1px #dddddd",
+        "height":"45px",
+        "width":"150px",
+        "border-radius":"20px",
+        "outline":"none"
+    });
+}
+
+function bb(){
+   alert("萨瓦迪卡！");
+   $("#username").val("hello  world");
+}
